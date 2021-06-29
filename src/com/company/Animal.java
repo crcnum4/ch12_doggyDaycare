d15c5e926be7177d2c37218543d4521bd96858b3
Cliff Choiniere <cliffchoiniere@Cliffs-MBP.fios-router.home>
2021-06-28 20:56:10 -0400

    adding animal fields and constructor

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,16 @@
 package com.company;
 
 public class Animal {
+    public int legs;
+    public String size;
+    public boolean isFixed;
+    public String name;
+    public double weight;
+
+    public Animal(int legs, String size, boolean isFixed, String name) {
+        this.legs = legs;
+        this.size = size;
+        this.name = name;
+        this.isFixed = isFixed;
+    }
 }
